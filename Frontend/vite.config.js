0b339e567874525b8a8032ae600c171d88a39c7f
Sonu Singh <shadow.pentest1899@gmail.com>
2026-05-19 23:52:36 +0530

    use vite plugin for mainfest

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    define: {
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['Union.png'],
+      manifest: {
+        name: 'Omnexa Chat',
+        short_name: 'Omnexa',
+        description: 'This is a real time chat app built by Sonu',
+        theme_color: '#f5f5f5',
+        background_color: '#f5f5f5',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/Union.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/Union.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
+  define: {
     global: 'globalThis',
   },
 })
