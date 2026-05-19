21144daec58bc9dc334a53be115c37c41b23f7e6
Sonu Singh <shadow.pentest1899@gmail.com>
2026-05-20 00:00:51 +0530

    change icon size

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['Union.png'],
+      includeAssets: ['icons/favicon.svg', 'icons/apple-icon-180.png'],
       manifest: {
         name: 'Omnexa Chat',
         short_name: 'Omnexa',
@@ -20,21 +19,18 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/Union.png',
+            src: '/icons/manifest-icon-192.maskable.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/Union.png',
+            src: '/icons/manifest-icon-512.maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
         ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
     })
   ],
